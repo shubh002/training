701531781cf452b6b372b5f2483f64b294425254
Shubham Verma <shubham96verma@gmail.com>
2019-01-29 11:30:54 +0530

    vid7 : Filters

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var app=angular.module('myApp', []);
 
 app.controller('MyController', ['$scope',function($scope){
-	$scope.fname='Shubham';
-	$scope.lname='Verma';
-	$scope.fullname=function(){
-		return $scope.fname + " " + $scope.lname;
-	};
-	$scope.isSpy=true;
-	$scope.codename='shub002';
+	$scope.employees = [
+	{
+		fname: 'Shubham',
+		lname: 'Verma',
+		salary: 5000,
+		reg: 47600123
+	},
+	{
+		fname: 'Anukool',
+		lname: 'Srivastava',
+		salary: 6000,
+		reg: 48000123
+	},
+	{
+		fname: 'Harshit',
+		lname: 'Gupta',
+		salary: 8000,
+		reg: 53600123
+	}
+	]
 }]);
