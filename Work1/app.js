950bbec744dc1df578b477d14e1bdd5ea55ccbe0
Shubham Verma <shubham96verma@gmail.com>
2019-01-30 02:36:55 +0530

    work1 v2.1

--- Work1/app.js
+++ Work1/app.js
@@ -6,32 +6,36 @@ app.controller('MyController', ['$scope',function($scope){
 		fname: 'Shubham',
 		lname: 'Verma',
 		salary: 5000,
-		reg: 47600123
+		regno: 47600123
 	},
 	{
 		fname: 'Anukool',
 		lname: 'Srivastava',
 		salary: 6000,
-		reg: 48000123
+		regno: 48000123
 	},
 	{
 		fname: 'Harshit',
 		lname: 'Gupta',
 		salary: 8000,
-		reg: 53600123
+		regno: 53600123
+	},
+	{
+		fname: 'Yash',
+		lname: 'Mittal',
+		salary: 4500,
+		regno: 46000123
+	},
+	{
+		fname: 'Kushal',
+		lname: 'Gupta',
+		salary: 4500,
+		regno: 43000123
 	}
 	]
-	
-	$scope.show0=!true;
-	$scope.show1=!true;
-	$scope.show2=!true;
-	$scope.toggleshow0 = function(){
-			$scope.show0= !$scope.show0;
-	};
-	$scope.toggleshow1 = function(){
-			$scope.show1= !$scope.show1;
-	};
-	$scope.toggleshow2 = function(){
-			$scope.show2= !$scope.show2;
+	$scope.count={};
+	$scope.show=!true;
+	$scope.toggleshow = function(){
+			$scope.show= !$scope.show;
 	};
 }]);
