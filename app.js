2196f54498174d4209d339c21531bd423d80a57f
Shubham Verma <shubham96verma@gmail.com>
2019-01-30 02:31:41 +0530

    Work1 v2

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
-var app=angular.module('myApp', ['reviewApp']);
+var app=angular.module('myApp', []);
 
+app.controller('MyController', ['$scope',function($scope){
+	$scope.data = [
+	{
+		fname: 'Shubham',
+		lname: 'Verma',
+		salary: 5000,
+		regno: 47600123
+	},
+	{
+		fname: 'Anukool',
+		lname: 'Srivastava',
+		salary: 6000,
+		regno: 48000123
+	},
+	{
+		fname: 'Harshit',
+		lname: 'Gupta',
+		salary: 8000,
+		regno: 53600123
+	},
+	{
+		fname: 'Yash',
+		lname: 'Mittal',
+		salary: 4500,
+		regno: 46000123
+	},
+	{
+		fname: 'Kushal',
+		lname: 'Gupta',
+		salary: 4500,
+		regno: 43000123
+	}
+	]
+	$scope.count={};
+	$scope.show=!true;
+	$scope.toggleshow = function(){
+			$scope.show= !$scope.show;
+	};
+}]);
