4688b8d05a270eebd75a03b2a46e2b70f418f445
Shubham Verma <shubham96verma@gmail.com>
2019-01-29 12:11:16 +0530

    vid9 : Form and ng-model

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var app=angular.module('myApp', []);
 
 app.controller('MyController', ['$scope',function($scope){
-	$scope.count=0;
-	$scope.show=true;
-	$scope.toggleshow = function(){
-			$scope.show= !$scope.show;
-	};
+	$scope.reviews = [
+	{
+		comment : 'Could this be more Awesome?',
+		by : 'Chandler Bing'
+	},
+	{
+		comment : 'How ya Doin?',
+		by : 'Joey Tribianni'
+	}
+	];
 }]);
