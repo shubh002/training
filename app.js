7a2879b66959ca813d19ab5dae468070c239cad2
Shubham Verma <shubham96verma@gmail.com>
2019-01-29 11:45:06 +0530

    vid 8 : ng-click

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 var app=angular.module('myApp', []);
 
 app.controller('MyController', ['$scope',function($scope){
-	$scope.employees = [
-	{
-		fname: 'Shubham',
-		lname: 'Verma',
-		salary: 5000,
-		reg: 47600123
-	},
-	{
-		fname: 'Anukool',
-		lname: 'Srivastava',
-		salary: 6000,
-		reg: 48000123
-	},
-	{
-		fname: 'Harshit',
-		lname: 'Gupta',
-		salary: 8000,
-		reg: 53600123
-	}
-	]
+	$scope.count=0;
+	$scope.show=true;
+	$scope.toggleshow = function(){
+			$scope.show= !$scope.show;
+	};
 }]);
