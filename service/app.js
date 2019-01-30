9083c6553bcbab6235ec701c4225706b99db61dc
Shubham Verma <shubham96verma@gmail.com>
2019-01-30 16:09:04 +0530

    Service : Service with callback function

--- service/app.js
+++ service/app.js
@@ -3,13 +3,16 @@ app.controller('cont1',['$scope', 'serv1' , function($scope, serv1){
 	$scope.a=10;
 	$scope.b=20;
 	$scope.doSum = function(){
-		$scope.sum=serv1.getSum($scope.a,$scope.b);		
+		serv1.getSum($scope.a,$scope.b, function(r){
+			$scope.sum=r;
+		});		
 	};
 }]);
 
 app.service('serv1',['$http','$log', function($http, $log){
 	$log.log("Instntiating serv1........")
-	this.getSum = function(a,b){
-		return parseInt(a) + parseInt(b);
+	this.getSum = function(a,b, cb){
+		var s = parseInt(a) + parseInt(b);
+		cb(s);
 	};
 }]);
