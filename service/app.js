70fccb3f51a49c20ee9b383145607bab887e2255
Shubham Verma <shubham96verma@gmail.com>
2019-01-30 16:05:29 +0530

    Service : service

--- service/app.js
+++ service/app.js
@@ -3,23 +3,13 @@ app.controller('cont1',['$scope', 'serv1' , function($scope, serv1){
 	$scope.a=10;
 	$scope.b=20;
 	$scope.doSum = function(){
-		//$scope.sum=serv1.getSum($scope.a,$scope.b);
-		serv1.getSum($scope.a,$scope.b,function(r){  //Callback Function
-			$scope.sum=r;
-		}); 
-			
+		$scope.sum=serv1.getSum($scope.a,$scope.b);		
 	};
 }]);
 
-app.factory('serv1',['$http','$log', function($http, $log){
+app.service('serv1',['$http','$log', function($http, $log){
 	$log.log("Instntiating serv1........")
-	var oserv1 = {};
-	//oserv1.getSum = function(a,b){
-	//	return parseInt(a)+parseInt(b);
-	//};
-	oserv1.getSum = function(a,b,cb){
-		var s = parseInt(a) + parseInt(b);
-		cb(s);
+	this.getSum = function(a,b){
+		return parseInt(a) + parseInt(b);
 	};
-	return oserv1;
 }]);
