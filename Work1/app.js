6f2c1af6a1815ab5d37c182cdc38a94618aca952
Shubham Verma <shubham96verma@gmail.com>
2019-02-12 10:57:50 +0530

    made static webpage

--- Work1/app.js
+++ Work1/app.js
@@ -1,41 +1,51 @@
 var app=angular.module('myApp', []);
 
-app.controller('MyController', ['$scope',function($scope){
-	$scope.data = [
-	{
-		fname: 'Shubham',
-		lname: 'Verma',
-		salary: 5000,
-		regno: 47600123
-	},
-	{
-		fname: 'Anukool',
-		lname: 'Srivastava',
-		salary: 6000,
-		regno: 48000123
-	},
-	{
-		fname: 'Harshit',
-		lname: 'Gupta',
-		salary: 8000,
-		regno: 53600123
-	},
-	{
-		fname: 'Yash',
-		lname: 'Mittal',
-		salary: 4500,
-		regno: 46000123
-	},
-	{
-		fname: 'Kushal',
-		lname: 'Gupta',
-		salary: 4500,
-		regno: 43000123
-	}
-	]
-	$scope.count={};
-	$scope.show=!true;
-	$scope.toggleshow = function(){
-			$scope.show= !$scope.show;
-	};
+app.controller('MyController', ['$scope', 'MyService',function($scope, myService){
+	
+	console.log(myService.getData());
+
+	$scope.candidates = myService.getData();
 }]);
+
+
+app.service('MyService', function(){
+
+	var myObject = {};
+	this.data = [
+		{
+			fname: 'Shubham',
+			lname: 'Verma',
+			salary: 5000,
+			regno: 47600123
+		},
+		{
+			fname: 'Anukool',
+			lname: 'Srivastava',
+			salary: 6000,
+			regno: 48000123
+		},
+		{
+			fname: 'Harshit',
+			lname: 'Gupta',
+			salary: 8000,
+			regno: 53600123
+		},
+		{
+			fname: 'Yash',
+			lname: 'Mittal',
+			salary: 4500,
+			regno: 46000123
+		},
+		{
+			fname: 'Kushal',
+			lname: 'Gupta',
+			salary: 5500,
+			regno: 43000123
+		}
+	];
+	
+	this.getData = function(){
+		return this.data;
+	};
+
+});
