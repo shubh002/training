152ccde0e5fedb69ca4c3c7e651c805e61bf3ecd
Shubham Verma <shubham96verma@gmail.com>
2019-02-13 15:03:08 +0530

    ngroute working

--- Work1/app.js
+++ Work1/app.js
@@ -1,12 +1,20 @@
 var app=angular.module('myApp', ['ngRoute']);
 
-app.controller('MyController', ['$scope', 'MyService',function($scope, myService){
+app.controller('MyController', ['$scope', 'MyService', '$location',function($scope, myService, $location){
 	
 	console.log(myService.getData());
 
 	$scope.candidates = myService.getData();
 
-	
+	$index=0;
+
+	$scope.retIndex = function(){
+		return $index;
+	};
+
+	$scope.showCandidateDetail = function(candidate){
+		$location.path('/viewCandidate/' +  candidate.fname);
+	}
 }]);
 
 
@@ -14,14 +22,15 @@ app.config(function($routeProvider){
 	$routeProvider
 		.when('/home', {
 			templateUrl: 'home.html',
-			controller: 'getData'
+			controller: 'MyController'
 		})
-		.when('/viewCandidate',{
+		.when('/viewCandidate/:name',{
 			templateUrl: 'viewCandidate.html',
-			controller: 'getData'
+			controller: 'MyController'
 		})
-		.
-
+		.otherwise({
+			redirectTo: '/home'
+		});
 });
 
 app.service('MyService', function(){
@@ -31,35 +40,42 @@ app.service('MyService', function(){
 			fname: 'Shubham',
 			lname: 'Verma',
 			salary: 5000,
-			regno: 47600123
+			regno: 47600123,
+			imagePath: '/images/img1.jpg'
+
 		},
 		{
 			fname: 'Anukool',
 			lname: 'Srivastava',
 			salary: 6000,
-			regno: 48000123
+			regno: 48000123,
+			imagePath: '/Work1/images/img1.jpg'
 		},
 		{
 			fname: 'Harshit',
 			lname: 'Gupta',
 			salary: 8000,
-			regno: 53600123
+			regno: 53600123,
+			imagePath: '/images/img1.jpg'
 		},
 		{
 			fname: 'Yash',
 			lname: 'Mittal',
 			salary: 4500,
-			regno: 46000123
+			regno: 46000123,
+			imagePath: '/images/img1.jpg'
 		},
 		{
 			fname: 'Kushal',
 			lname: 'Gupta',
 			salary: 5500,
-			regno: 43000123
+			regno: 43000123,
+			imagePath: '/images/img1.jpg'
 		}
 	];
 	
-	this.getData = function(){
+	var cand={};
+	this.getData = function(cand){
 		return this.data;
 	};
 
