8060f70bc47b2c45423f45e424d9bc7f81fc8123
Shubham Verma <shubham96verma@gmail.com>
2019-02-14 13:58:55 +0530

    working prototype Work1

--- Work1/app.js
+++ Work1/app.js
@@ -1,21 +1,24 @@
 var app=angular.module('myApp', ['ngRoute']);
 
-app.controller('CandController', ['$scope', 'MyService',function($scope, myService){
-	
+app.controller('CandController', ['$scope', 'MyService', function($scope, myService){
+
 	console.log(myService.getData());
 
 	$scope.candidates = myService.getData();
-
+	
 	$scope.index=0;
 
 }]);
 
-app.controller('DisplayDetails',['$scope', 'MyService',function($scope, myService){
+app.controller('DisplayDetails',['$scope', 'MyService', '$routeParams',function($scope, myService, $routeParams){
+	
 	$scope.candidates = myService.getData();
+	console.log($routeParams.regNo);
 
-	$scope.showCandidateDetail = function($candidate){
-			return $candidate;
-	};
+	
+	var regNo = parseInt($routeParams.regNo);
+	
+	$scope.singleCand = myService.getCandidate(regNo);
 
 }]);
 
@@ -25,7 +28,7 @@ app.config(function($routeProvider){
 			templateUrl: 'home.html',
 			controller: 'CandController'
 		})
-		.when('/viewCandidate/:name',{
+		.when('/viewCandidate/:regNo',{
 			templateUrl: 'viewCandidate.html',
 			controller: 'DisplayDetails'
 		})
@@ -41,7 +44,7 @@ app.service('MyService', function(){
 			fname: 'Shubham',
 			lname: 'Verma',
 			salary: 5000,
-			regno: 47600123,
+			regNo: 47600123,
 			imagePath: '/Work1/images/male.jpg'
 
 		},
@@ -49,48 +52,54 @@ app.service('MyService', function(){
 			fname: 'Anukool',
 			lname: 'Srivastava',
 			salary: 6000,
-			regno: 48000123,
+			regNo: 48000123,
 			imagePath: '/Work1/images/male.jpg'
 		},
 		{
 			fname: 'Surabhi',
 			lname: 'Sethi',
 			salary: 5000,
-			regno: 42000123,
+			regNo: 42000123,
 			imagePath: '/Work1/images/female.jpg'
 		},
 		{
 			fname: 'Harshit',
 			lname: 'Gupta',
 			salary: 8000,
-			regno: 53600123,
+			regNo: 53600123,
 			imagePath: '/Work1/images/male.jpg'
 		},
 		{
 			fname: 'Archana',
 			lname: 'Nair',
 			salary: 7500,
-			regno: 44700123,
+			regNo: 44700123,
 			imagePath: '/Work1/images/female.jpg'
 		},
 		{
 			fname: 'Yash',
 			lname: 'Mittal',
 			salary: 4500,
-			regno: 46000123,
+			regNo: 46000123,
 			imagePath: '/Work1/images/male.jpg'
 		},
 		{
 			fname: 'Kushal',
 			lname: 'Gupta',
 			salary: 5500,
-			regno: 43000123,
+			regNo: 43000123,
 			imagePath: '/Work1/images/male.jpg'
 		}
 	];
 	
 	this.getData = function(){
 		return this.data;
+	};
+
+	this.getCandidate = function(regNo){
+		return this.data.filter(function(singleCand){
+			return singleCand.regNo === regNo;
+		})[0];
 	};
 
 });
